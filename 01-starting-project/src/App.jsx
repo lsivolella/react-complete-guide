1ce68f5be754823a3a367e95c9028fad9ea6dbc7
Lucas <lucassivolella@gmail.com>
2026-09-09 15:33:44 -0300

    Introduce a new menu with dynamic content and a tabs system

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from 'react';
+
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcepts/CoreConcepts.jsx';
+import TabButton from './components/TabButton/TabButton.jsx';
 
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 
+const componentsText = 'components';
+const jsxText = 'jsx';
+const propsText = 'props';
+const stateText = 'state';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>"Please select a topic."</p>;
+
+  if (selectedTopic) {
+    tabContent =
+      (<div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>);
+  }
+
   return (
     <div>
       <Header></Header>
@@ -21,6 +49,27 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              onClick={() => handleClick(componentsText)}
+              isSelected={selectedTopic === componentsText}
+            >Components</TabButton>
+            <TabButton
+              onClick={() => handleClick(jsxText)}
+              isSelected={selectedTopic === jsxText}
+            >JSX</TabButton>
+            <TabButton
+              onClick={() => handleClick(propsText)}
+              isSelected={selectedTopic === propsText}
+            >Props</TabButton>
+            <TabButton onClick={() => handleClick(stateText)}
+              isSelected={selectedTopic === stateText}
+            >State</TabButton>
+          </menu>
+          {tabContent}
         </section>
       </main>
     </div>
