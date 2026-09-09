de7887b294d7ff68fea3b451287d3cc4ac6e95e0
Lucas <lucassivolella@gmail.com>
2026-09-09 17:17:14 -0300

    Refactor Examples code to a new component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,65 +1,16 @@
-import { useState } from 'react';
-
 import Header from './components/Header/Header.jsx';
-import TabButton from './components/TabButton/TabButton.jsx';
-
-import { CORE_CONCEPTS, EXAMPLES } from './data';
-
-const componentsText = 'components';
-const jsxText = 'jsx';
-const propsText = 'props';
-const stateText = 'state';
+import Examples from './components/Examples/Examples.jsx';
 import CoreConcepts from './components/CoreConcepts/CoreConcepts.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleClick(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
-  let tabContent = <p>"Please select a topic."</p>;
-
-  if (selectedTopic) {
-    tabContent =
-      (<div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>
-            {EXAMPLES[selectedTopic].code}
-          </code>
-        </pre>
-      </div>);
-  }
-
   return (
-    <div>
-      <Header></Header>
+    <>
+      <Header />
       <main>
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              onClick={() => handleClick(componentsText)}
-              isSelected={selectedTopic === componentsText}
-            >Components</TabButton>
-            <TabButton
-              onClick={() => handleClick(jsxText)}
-              isSelected={selectedTopic === jsxText}
-            >JSX</TabButton>
-            <TabButton
-              onClick={() => handleClick(propsText)}
-              isSelected={selectedTopic === propsText}
-            >Props</TabButton>
-            <TabButton onClick={() => handleClick(stateText)}
-              isSelected={selectedTopic === stateText}
-            >State</TabButton>
-          </menu>
-          {tabContent}
-        </section>
         <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
---
+++ 01-starting-project/src/components/Examples/Examples.jsx
@@ -0,0 +1,58 @@
+import './Examples.css'
+
+import { useState } from 'react';
+import TabButton from '../../components/TabButton/TabButton.jsx';
+
+import { EXAMPLES } from '../../data';
+
+export default function Examples() {
+    const [selectedTopic, setSelectedTopic] = useState();
+
+    const componentsText = 'components';
+    const jsxText = 'jsx';
+    const propsText = 'props';
+    const stateText = 'state';
+
+    function handleClick(selectedButton) {
+        setSelectedTopic(selectedButton);
+    }
+
+    let tabContent = <p>"Please select a topic."</p>;
+
+    if (selectedTopic) {
+        tabContent =
+            (<div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>
+                        {EXAMPLES[selectedTopic].code}
+                    </code>
+                </pre>
+            </div>);
+    }
+
+    return (
+        <section id="examples">
+            <h2>Examples</h2>
+            <menu>
+                <TabButton
+                    onClick={() => handleClick(componentsText)}
+                    isSelected={selectedTopic === componentsText}
+                >Components</TabButton>
+                <TabButton
+                    onClick={() => handleClick(jsxText)}
+                    isSelected={selectedTopic === jsxText}
+                >JSX</TabButton>
+                <TabButton
+                    onClick={() => handleClick(propsText)}
+                    isSelected={selectedTopic === propsText}
+                >Props</TabButton>
+                <TabButton onClick={() => handleClick(stateText)}
+                    isSelected={selectedTopic === stateText}
+                >State</TabButton>
+            </menu>
+            {tabContent}
+        </section>
+    );
+}
